98f07f4ea423d46151467a0f2f7756316ec82300
Elyas Alqasim <>
2026-06-14 15:41:09 +0300

    Add login functionality

--- chargelens.py
+++ chargelens.py
@@ -1,13 +1,16 @@
 import os
 
 import pandas as pd
-from flask import Flask, request, render_template, redirect, url_for, send_file
+from functools import wraps
+from flask import Flask, request, session, render_template, redirect, url_for, send_file
 from sqlalchemy import inspect, text
 from io import BytesIO
+from werkzeug.security import check_password_hash
 
 from database import db, DB_USER, DB_PASS, DB_NAME, INSTANCE_CONNECTION_NAME
 from data_importer import import_data_files
 from db_query import get_table, create_indexes
+
 
 
 app = Flask(__name__)
@@ -15,14 +18,50 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}"
     f"?host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
 )
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
+app.secret_key = os.environ.get("SECRET_KEY")
 db.init_app(app)
 
+def login_required(route_function):
+    @wraps(route_function)
+    def wrapper(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        return route_function(*args, **kwargs)
+    return wrapper
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if session.get("logged_in"):
+        return redirect(url_for("home"))
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == ADMIN_USERNAME and check_password_hash(ADMIN_PASSWORD, password):
+            session["logged_in"] = True
+            return redirect(url_for("home"))
+
+        return render_template("login.html", error="Invalid username or password")
+    
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
 @app.route("/")
+@login_required
 def home():
     print(app.config["MAX_CONTENT_LENGTH"])
     return render_template("home.html")
 
 @app.route("/upload-folder", methods=["GET", "POST"])
+@login_required
 def upload_folder():
     if request.method == "POST":
         folder_path = request.form["folder_path"]
@@ -49,6 +88,7 @@ def upload_folder():
         return render_template("upload_folder.html")
 
 @app.route("/search", methods=["GET", "POST"])
+@login_required
 def search():
     if request.method == "POST":
         search_type = request.form.get("search_type")
@@ -68,6 +108,7 @@ def search():
 
         
 @app.route("/view_tables")
+@login_required
 def view_tables():
     inspector = inspect(db.engine)
     tables = inspector.get_table_names()
@@ -75,6 +116,7 @@ def view_tables():
     return render_template("tables.html", tables=tables)
 
 @app.route("/delete-table/<table_name>", methods=["POST"])
+@login_required
 def delete_table(table_name):
     inspector = inspect(db.engine)
     tables = inspector.get_table_names()
@@ -88,6 +130,7 @@ def delete_table(table_name):
     return redirect(url_for("view_tables"))
 
 @app.route("/result")
+@login_required
 def result():
     search_type = request.args.get("search_type")
 
@@ -112,6 +155,7 @@ def result():
     )
 
 @app.route("/export")
+@login_required
 def export_results():
     search_type = request.args.get("search_type")
 
